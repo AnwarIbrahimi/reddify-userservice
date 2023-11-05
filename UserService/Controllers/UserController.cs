173a0e05fa9ceca02ee31ba69d17bf43ced6e8d1
Anwar <465821@student.fontys.nl>
2023-11-05 17:29:00 +0100

    added userservice project

---
+++ UserService/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UserService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
