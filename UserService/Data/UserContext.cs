428a8781abf8cacbe235730162e55a46a2c974ca
Anwar <465821@student.fontys.nl>
2023-11-22 11:06:41 +0100

    added data migration

---
+++ UserService/Data/UserContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserService.Models;
+
+namespace UserService.Data
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=mssqlstud.fhict.local;Database=dbi465821_content;User Id=dbi465821;Password=Voucugklir2;");
+        }
+    }
+}
